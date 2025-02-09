49849391bc40ad7e8cc7cca043a77e0d6166939e
Kostya Lachin <prikolist04@mail.ru>
2025-02-09 20:16:02 +0300

    Reformate code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-<<<<<<< HEAD
+
         return   price / 20;
     }
 }
-=======
-            return price / 20; 
-}
->>>>>>> 6987a011b030166479f88ce50089e0cce3bcab7c
